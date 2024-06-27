0290367e0bd04a40ae58b6f2e3b679e8c82838ef
sonubhosle <sbhosle1011@gmil.com>
2024-06-28 00:13:28 +0530

    28 jun

--- src/Components/Products/Home_Products.jsx
+++ src/Components/Products/Home_Products.jsx
@@ -1,17 +1,38 @@
-import React from 'react'
-import Card from './Card'
-import './Style.css'
-const Home_Products = ({products,heading}) => {
-  return (
-    <> 
-    <div className="section_heading">{heading}</div>
-    <div className='products_grid' >
-      {
-        products.map((item,index) =><Card product={item} indx={index} />)
-      }
-    </div>
-    </>
-  )
-}
+import React, { useState } from 'react';
+import Card from './Card';
+import './Style.css';
+import { FiSearch } from 'react-icons/fi';
 
-export default Home_Products
+const Home_Products = ({ products, heading }) => {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  return (
+    <>
+      <div className="search_bar">
+        <input
+          type="text"
+          placeholder="Search products"
+          onChange={(e) => setSearchTerm(e.target.value)}
+        />
+       <button> <FiSearch /></button>
+      </div>
+      <div className="section_heading">{heading}</div>
+      <div className="products_grid">
+        {products
+          .filter((prod) => {
+            if (searchTerm === '') {
+              return prod;
+            } else if (prod.title.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return prod;
+            }
+            return false;
+          })
+          .map((item, index) => (
+            <Card product={item} indx={index} key={index} />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default Home_Products;
